6d6363dcc8da04847f0783a25608024814533faf
anotre <notrevispe@yandex.ru>
2024-05-06 21:12:24 +0500

    hw05-jdbc initial

---
+++ hw05-jdbc/src/main/java/ru/otus/hw/Application.java
@@ -0,0 +1,15 @@
+package ru.otus.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.h2.tools.Console;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+}
