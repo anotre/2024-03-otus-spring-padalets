689c2185d66dc901ddb1a460cacae491c497d141
anotre <notrevispe@yandex.ru>
2024-05-07 21:17:56 +0500

    hw05-jdbc authors and genres repository added

--- hw05-jdbc/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,42 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+    private final NamedParameterJdbcOperations jdbc;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return jdbc.getJdbcOperations().query("select id, name from authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        var params = Collections.singletonMap("id", id);
+        var author = jdbc.queryForObject("select id, name from authors where id = :id", params, new AuthorRowMapper());
+        return Optional.ofNullable(author);
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Author(
+                    rs.getLong("id"),
+                    rs.getString("name")
+            );
         }
     }
 }
--- hw05-jdbc/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05-jdbc/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,42 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcGenreRepository implements GenreRepository {
+    private final NamedParameterJdbcOperations jdbc;
 
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        return jdbc.getJdbcOperations().query("select id, name from genres", new GenreRowMapper());
     }
 
     @Override
     public Optional<Genre> findById(long id) {
-        return Optional.empty();
+        var params = Collections.singletonMap("id", id);
+        var genres = jdbc.queryForObject("select id, name from genres where id = :id", params, new GenreRowMapper());
+        return Optional.ofNullable(genres);
     }
 
-    private static class GnreRowMapper implements RowMapper<Genre> {
+    private static class GenreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Genre(
+                    rs.getLong("id"),
+                    rs.getString("name")
+            );
         }
     }
 }
