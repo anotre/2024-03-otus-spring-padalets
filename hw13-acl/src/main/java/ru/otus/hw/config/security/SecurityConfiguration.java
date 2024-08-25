3625131e549c8fa9eff0149f0e3b312f994261db
anotre <notrevispe@yandex.ru>
2024-08-25 15:55:38 +0500

    hw13-acl fix final

--- hw13-acl/src/main/java/ru/otus/hw/config/security/SecurityConfiguration.java
+++ hw13-acl/src/main/java/ru/otus/hw/config/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package ru.otus.hw.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,16 +24,6 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/books", "/books/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                        .requestMatchers(HttpMethod.POST, "/books/delete/{id}").access(
-                                new WebExpressionAuthorizationManager(
-                                        "hasPermission(#id, 'ru.otus.hw.models.Book', 'DELETE') || " +
-                                                "hasAuthority('ROLE_ADMIN')")
-                        )
-                        .requestMatchers(HttpMethod.POST, "/books/edit").access(
-                                new WebExpressionAuthorizationManager(
-                                        "hasPermission(#id, 'ru.otus.hw.models.Book', 'WRITE') || " +
-                                                "hasAuthority('ROLE_ADMIN')")
-                        )
                         .anyRequest().authenticated())
                 .formLogin(fm -> fm
                         .loginPage("/login")
